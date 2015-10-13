33bdf9fe0f18e458be367d1d224c55242ca79590
DmitryGlazentsov <dimaglazentsov@gmail.com>
2015-10-13 03:21:36 +0300

    Added some table

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
-	$("body").append("<div>Working!</div>");
+	$("body").append("<div class='main_div'></div>");
+	var w=16;//ширина (количество клеток)
+	var h=16;//высота (количество клеток)
+	
+	//пока что так, потому что нельзя называть классы цифрами
+	var name_of_class = ["one","two","three","four",
+						"five","six","seven","eight",
+						"nine","ten","eleven","twelve",
+						"thirteen","fourteen","fifteen","sixteen"];
+	
+	//для наглядности делаем рамку дива
+	$(".one").css("border","3px solid black");
+	//создаем сетку дивов
+	for (var i = 0; i < w; i++) {
+		for (var k = 0; k < h; k++) {
+			//$(".main_div").append("<div class='"+name_of_class[i]+"'>"+k+"-столбец "+i +"-строка"+"</div>");
+			$(".main_div").append("<div class='"+name_of_class[i]+"'>"+k+" "+i+"</div>");
+			if(k==15){
+				$(".main_div").append("<div>________________________________________________________</div>");
+			};
+		};
+	};
+
+	//задаем параметры отображения блоков div
+	for (var i = 0; i < name_of_class.length; i++) {
+		$("."+name_of_class[i]).css("display","inline-table");
+		$("."+name_of_class[i]).css("border","3px solid black");
+		$("."+name_of_class[i]).css("padding","5px");
+		$("."+name_of_class[i]).css("margin","2px");
+		$("."+name_of_class[i]).css("max-width","30px");
+		$("."+name_of_class[i]).css("max-height","30px");
+		$("."+name_of_class[i]).css("min-width","5px");
+		$("."+name_of_class[i]).css("min-height","5px");
+	};
+
 });
